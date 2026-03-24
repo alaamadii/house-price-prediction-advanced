e3a310ef99692df8d95d1468d27d325d6effa675
Alaa <alaamadi005@gmail.com>
2026-03-24 13:08:32 +0300

    Delete src/main.py

--- src/main.py
+++
@@ -1,8 +0,0 @@
-from data_loader import load_data
-from preprocessing import preprocess_data
-from model import train_models
-
-
-df = load_data("data/train.csv")
-x_train, x_test,y_train,y_test = preprocess_data(df)
-train_models(x_train,x_test,y_train,y_test)
